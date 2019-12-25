eae834d319c3956487208848b0c62fc95303705e
riyathebird <sannikova.k@gmail.com>
2019-12-25 21:59:06 +0300

    modified test connected to pages

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,13 @@
+from .pages.main_page import MainPage
+
+link = "http://selenium1py.pythonanywhere.com/"
+
+def go_to_login_page(browser):
+    link = browser.find_element_by_css_selector("#login_link")
+    link.click()
+
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
